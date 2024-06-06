5cd5d3ac008d16cbb73645e68fb0727b1289af75
Yeasin00p <07Yeasin1981@gmail.com>
2024-06-06 16:46:48 +0600

    add expense page update

--- lib/screen/home/add_expences/views/add_expense.dart
+++ lib/screen/home/add_expences/views/add_expense.dart
@@ -14,6 +14,16 @@ class _AddExpenseState extends State<AddExpense> {
   TextEditingController categoryController = TextEditingController();
   TextEditingController dateController = TextEditingController();
   DateTime selectDate = DateTime.now();
+  List<String> myCategoriesIcon = [
+    'entertainment',
+    'food',
+    'home',
+    'pet',
+    'shopping',
+    'tech',
+    'travel',
+  ];
+  String iconSelected = '';
 
   @override
   void initState() {
@@ -72,6 +82,7 @@ class _AddExpenseState extends State<AddExpense> {
               TextFormField(
                 textAlignVertical: TextAlignVertical.center,
                 controller: categoryController,
+                readOnly: true,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
@@ -79,6 +90,166 @@ class _AddExpenseState extends State<AddExpense> {
                     FontAwesomeIcons.list,
                     size: 16,
                     color: Colors.grey,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          bool isExpended = false;
+                          return StatefulBuilder(builder: (context, setState) {
+                            return AlertDialog(
+                              title: const Text('Create a category'),
+                              content: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextFormField(
+                                      textAlignVertical:
+                                          TextAlignVertical.center,
+                                      decoration: InputDecoration(
+                                        isDense: true,
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        prefixIcon: const Icon(
+                                          FontAwesomeIcons.calendar,
+                                          size: 16,
+                                          color: Colors.grey,
+                                        ),
+                                        hintText: 'Name',
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 16,
+                                    ),
+                                    TextFormField(
+                                      onTap: () {
+                                        setState(() {
+                                          isExpended = !isExpended;
+                                        });
+                                      },
+                                      textAlignVertical:
+                                          TextAlignVertical.center,
+                                      readOnly: true,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        isDense: true,
+                                        fillColor: Colors.white,
+                                        prefixIcon: const Icon(
+                                          FontAwesomeIcons.calendar,
+                                          size: 16,
+                                          color: Colors.grey,
+                                        ),
+                                        suffixIcon: const Icon(
+                                            FontAwesomeIcons.chevronDown),
+                                        hintText: 'Icon',
+                                        border: OutlineInputBorder(
+                                          borderRadius: isExpended
+                                              ? const BorderRadius.vertical(
+                                                  top: Radius.circular(12),
+                                                )
+                                              : BorderRadius.circular(12),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                    isExpended
+                                        ? Container(
+                                            width: MediaQuery.of(context)
+                                                .size
+                                                .width,
+                                            height: 200,
+                                            decoration: const BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.vertical(
+                                                bottom: Radius.circular(12),
+                                              ),
+                                            ),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: GridView.builder(
+                                                gridDelegate:
+                                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                                  crossAxisCount: 3,
+                                                  mainAxisSpacing: 5,
+                                                  crossAxisSpacing: 5,
+                                                ),
+                                                itemCount:
+                                                    myCategoriesIcon.length,
+                                                itemBuilder: (context, int i) {
+                                                  return GestureDetector(
+                                                    onTap: () {
+                                                      setState(() {
+                                                        iconSelected =
+                                                            myCategoriesIcon[i];
+                                                      });
+                                                    },
+                                                    child: Container(
+                                                      decoration: BoxDecoration(
+                                                        border: Border.all(
+                                                          width: 5,
+                                                          color: iconSelected ==
+                                                                  myCategoriesIcon[i]
+                                                              ? Colors.green
+                                                              : Colors.grey,
+                                                        ),
+                                                        image: DecorationImage(
+                                                          image: AssetImage(
+                                                            'assets/icon/${myCategoriesIcon[i]}.png',
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  );
+                                                },
+                                              ),
+                                            ),
+                                          )
+                                        : Container(),
+                                    const SizedBox(
+                                      height: 16,
+                                    ),
+                                    TextFormField(
+                                      textAlignVertical:
+                                          TextAlignVertical.center,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        isDense: true,
+                                        fillColor: Colors.white,
+                                        prefixIcon: const Icon(
+                                          FontAwesomeIcons.calendar,
+                                          size: 16,
+                                          color: Colors.grey,
+                                        ),
+                                        hintText: 'Color',
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                        },
+                      );
+                    },
+                    icon: const Icon(
+                      FontAwesomeIcons.list,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
                   ),
                   hintText: 'Category',
                   border: OutlineInputBorder(
